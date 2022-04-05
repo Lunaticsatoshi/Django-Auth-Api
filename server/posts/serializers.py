de8f6b1b9f0f6c4f82ba612f73197724460b04cb
Lunaticsatoshi <deysarkersudeep@gmail.com>
2022-04-05 21:11:29 +0530

    :jigsaw: Creating post comment serializer

--- server/posts/serializers.py
+++ server/posts/serializers.py
@@ -5,10 +5,21 @@ from user.serializers import UserSerializer
 from .models import Post, PostComment
 
 
-class ArticleSerializer(ModelSerializer):
+class PostSerializer(ModelSerializer):
     user = serializers.SerializerMethodField(read_only=True)
     class Meta:
         model = Post
+        fields = '__all__'
+        
+    def get_user(self, obj):
+        user = obj.user
+        serializer = UserSerializer(user, many=False)
+        return serializer.data
+    
+class PostCommentSerializer(ModelSerializer):
+    user = serializers.SerializerMethodField(read_only=True)
+    class Meta:
+        model = PostComment
         fields = '__all__'
         
     def get_user(self, obj):
