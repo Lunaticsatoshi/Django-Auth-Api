7fef3c742175456e29ef5ddf56ee36ee9c855f06
Lunaticsatoshi <deysarkersudeep@gmail.com>
2022-04-05 20:05:09 +0530

    :hammer: Adding init admin script to create admin user on start

---
+++ server/users/management/commands/initadmin.py
@@ -0,0 +1,15 @@
+from django.conf import settings
+from django.core.management.base import BaseCommand
+from users.models import CustomUser
+
+class Command(BaseCommand):
+    def handle(self, *args, **options):
+        if not CustomUser.objects.filter(email=settings.ADMIN_EMAIL).exists():
+            CustomUser.objects.create_superuser(
+                username=settings.ADMIN_EMAIL,
+                email=settings.ADMIN_EMAIL,
+                password=settings.ADMIN_PASSWORD,
+            )
+            print('Admin account created successfully')
+        else:
+            print('Admin account already exists')
