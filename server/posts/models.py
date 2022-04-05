acb6719f9224dfeac24f3e5a01cf6ab7a8e1fb0e
Lunaticsatoshi <deysarkersudeep@gmail.com>
2022-04-05 20:51:44 +0530

    :bricks: Created a post model to handle post data

--- server/posts/models.py
+++ server/posts/models.py
@@ -1,3 +1,18 @@
+import uuid
 from django.db import models
 
+from api.models import BaseModel
+from users.models import CustomUser
+
 # Create your models here.
+class Post(BaseModel):
+    id = models.UUIDField(default=uuid.uuid4,  unique=True, primary_key=True, editable=False)
+    user = models.ForeignKey(CustomUser, on_delete=models.SET_NULL, null=True, blank=True)
+    title = models.CharField(max_length=700, db_index=True)
+    content = models.TextField(max_length=10000)
+    article_image=models.CharField(max_length=555, null=True, blank=True)
+    slug = models.SlugField(max_length=700, unique=True, db_index=True)
+    comment_count = models.IntegerField(null=True, default=0)
+    
+    def __str__(self) -> str:
+        return f"{self.id} | {self.title}"
