9817f672d9dfb2c17329c6e69fbf8b879b7d4112
Lunaticsatoshi <deysarkersudeep@gmail.com>
2022-04-05 21:19:30 +0530

    :gear: Adding create post api view

--- server/posts/views.py
+++ server/posts/views.py
@@ -1,5 +1,4 @@
 from django.shortcuts import render
-from django.views.decorators.cache import cache_page
 
 from rest_framework.response import Response
 from rest_framework.generics import GenericAPIView
@@ -7,23 +6,49 @@ from rest_framework.decorators import api_view, permission_classes
 from rest_framework import status
 from rest_framework.permissions import IsAuthenticated
 
-from .serializers import ArticleSerializer, ArticleCommentSerializer
-from .models import Article, HashTag, ArticleClap, ArticleComment
+from .serializers import PostSerializer, PostCommentSerializer
+from .models import Post, PostComment
 from .utils import Utils
 from .permissions import IsOwner
 
 # Create your views here.
 class UserPostListApiView(GenericAPIView):
-    serializer_class = ArticleSerializer
-    queryset = Article.objects.all()
+    serializer_class = PostSerializer
+    queryset = Post.objects.all()
     permission_classes = (IsAuthenticated, IsOwner)
     
     
     def get(self, request):
         user = request.user
         try:
-            articles = Article.objects.filter(user=user)
-            serializer = ArticleSerializer(articles, many=True)
+            posts = Post.objects.filter(user=user)
+            serializer = PostSerializer(posts, many=True)
             return Response({ 'message': 'Articles retrieved sucessfully', 'data': serializer.data }, status=status.HTTP_200_OK)
         except Exception as e:
             return Response({'message': 'Something went wrong'}, status=status.HTTP_400_BAD_REQUEST)
+        
+class UserPostCreateApiView(GenericAPIView):
+    serializer_class = PostSerializer
+    permission_classes = (IsAuthenticated, IsOwner)
+    
+    def post(self, request):
+        user = request.user
+        data = request.data
+        title = data.get('title')
+        content = data.get('content')
+        try:
+            if not title or not content:
+                return Response({'message': 'title and content is required'}, status=status.HTTP_400_BAD_REQUEST)
+            
+            slug = Utils.generate_slug(title)
+            
+            post = Post.objects.create(title=title, content=content, slug=slug, user=user)
+                
+            post.save()
+            serializer = PostSerializer(post, many=False)
+
+            return Response({ 'message': 'Post created successfully', 'post': serializer.data })
+        
+        except Exception as e:
+            print(e)
+            return Response({ 'message': 'Something went wrong' }, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
