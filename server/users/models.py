8783e75c40fccdd584671365daa70cd275a0b9db
Lunaticsatoshi <deysarkersudeep@gmail.com>
2022-04-05 19:52:18 +0530

    :bricks: Creating a user model to handle user data

--- server/users/models.py
+++ server/users/models.py
@@ -1,3 +1,44 @@
 from django.db import models
+from django.contrib.auth.models import (BaseUserManager, AbstractBaseUser, PermissionsMixin)
 
-# Create your models here.
+# Create custom user manager.
+class UserManager(BaseUserManager):
+
+    def create_user(self, username, email, password=None):
+        if username is None:
+            raise TypeError('Users should have a username')
+        if email is None:
+            raise TypeError('Users should have a Email')
+
+        user = self.model(username=username, email=self.normalize_email(email))
+        user.set_password(password)
+        user.save()
+        return user
+
+    def create_superuser(self, username, email, password=None):
+        if password is None:
+            raise TypeError('Password should not be none')
+
+        user = self.create_user(username, email, password)
+        user.role = 'Admin'
+        user.is_superuser = True
+        user.is_staff = True
+        user.save()
+        return user
+    
+class CustomUser(AbstractBaseUser, PermissionsMixin):
+    username = models.CharField(max_length=100, unique=True, db_index=True)
+    email = models.EmailField(max_length=255, unique=True, db_index=True)
+    email_verified = models.BooleanField(default=False)
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    objects = UserManager()
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['username']
+
+    def __str__(self) -> str:
+        return f"{self.username} | {self.id}"
