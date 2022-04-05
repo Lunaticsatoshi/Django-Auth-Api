9fd65fc90de472736d7a58ee627909950ca824f8
Lunaticsatoshi <deysarkersudeep@gmail.com>
2022-04-05 21:14:37 +0530

    :gear: Adding get post view

--- server/posts/views.py
+++ server/posts/views.py
@@ -1,3 +1,29 @@
 from django.shortcuts import render
+from django.views.decorators.cache import cache_page
+
+from rest_framework.response import Response
+from rest_framework.generics import GenericAPIView
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework import status
+from rest_framework.permissions import IsAuthenticated
+
+from .serializers import ArticleSerializer, ArticleCommentSerializer
+from .models import Article, HashTag, ArticleClap, ArticleComment
+from .utils import Utils
+from .permissions import IsOwner
 
 # Create your views here.
+class UserPostListApiView(GenericAPIView):
+    serializer_class = ArticleSerializer
+    queryset = Article.objects.all()
+    permission_classes = (IsAuthenticated, IsOwner)
+    
+    
+    def get(self, request):
+        user = request.user
+        try:
+            articles = Article.objects.filter(user=user)
+            serializer = ArticleSerializer(articles, many=True)
+            return Response({ 'message': 'Articles retrieved sucessfully', 'data': serializer.data }, status=status.HTTP_200_OK)
+        except Exception as e:
+            return Response({'message': 'Something went wrong'}, status=status.HTTP_400_BAD_REQUEST)
