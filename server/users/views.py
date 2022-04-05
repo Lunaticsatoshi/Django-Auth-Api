73e9e9456461cf139adb8101626bf2beb68becdd
Lunaticsatoshi <deysarkersudeep@gmail.com>
2022-04-05 20:17:25 +0530

    :jigsaw: Adding register user view

--- server/users/views.py
+++ server/users/views.py
@@ -1,3 +1,37 @@
 from django.shortcuts import render
+from django.contrib.sites.shortcuts import get_current_site
+from django.urls import reverse
+from django.conf import settings
+from django.db.models import Q
+import jwt
+
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from rest_framework.generics import CreateAPIView, UpdateAPIView
+from rest_framework import status
+from rest_framework_simplejwt.tokens import RefreshToken, TokenError
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.permissions import AllowAny, IsAuthenticated
+from rest_framework_simplejwt.views import TokenObtainPairView
+
+from drf_yasg.utils import swagger_auto_schema
+from drf_yasg import openapi
+
+from .serializers import RegisterSerializer, LoginSerializerWithToken
+from .models import CustomUser, Interests
 
 # Create your views here.
+
+class RegisterView(CreateAPIView):
+    serializer_class = RegisterSerializer
+    permission_classes = (AllowAny,)
+    
+    def post(self, request):
+        data = request.data
+        serializer = self.serializer_class(data=data)
+        
+        if serializer.is_valid(raise_exception=True):
+            serializer.save()
+            user_data = serializer.data
+            
+            return Response({'message': 'User created successfully', 'data': user_data}, status=status.HTTP_201_CREATED)
