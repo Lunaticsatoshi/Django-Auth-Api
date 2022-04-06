8d301dd3a2bb72508b064608c5a01a84cc6ca5b5
Lunaticsatoshi <deysarkersudeep@gmail.com>
2022-04-06 20:45:14 +0530

    :jigsaw: Updating user serializer import in post serializer

--- server/posts/serializers.py
+++ server/posts/serializers.py
@@ -1,7 +1,7 @@
 from rest_framework import serializers
 from rest_framework.serializers import ModelSerializer
 
-from user.serializers import UserSerializer
+from users.serializers import UserSerializer
 from .models import Post, PostComment
 
 
