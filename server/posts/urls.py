d03e134b15ad8dce9523e02b007511894981bfbd
Lunaticsatoshi <deysarkersudeep@gmail.com>
2022-04-06 20:49:00 +0530

    :railway_track: Updating post views to url routes

--- server/posts/urls.py
+++ server/posts/urls.py
@@ -1,13 +1,14 @@
 from django.urls import path
-from .views import UserArticleListApiView, UserArticleCreateApiView, UserArticleUpdateApiView, UserArticleDetailApiView, UserArticleDeleteApiView, get_article, get_articles, add_comment
+from .views import UserPostListApiView, UserPostCreateApiView, UserPostUpdateApiView, UserPostDetailApiView, UserPostDeleteApiView, get_post, get_posts, get_comments, add_comment
 
 urlpatterns = [
-    path('user/all/', UserArticleListApiView.as_view(), name='user-article-list'),
-    path('user/<str:id>', UserArticleDetailApiView.as_view(), name='user-article-detail'),
-    path('create/', UserArticleCreateApiView.as_view(), name='user-article-create'),
-    path('<str:id>/update/', UserArticleUpdateApiView.as_view(), name='user-article-update'),
-    path('<str:id>/delete/', UserArticleDeleteApiView.as_view(), name='user-article-delete'),
-    path('all/', get_articles, name='all-articles'),
+    path('user/all/', UserPostListApiView.as_view(), name='user-post-list'),
+    path('user/<str:id>/', UserPostDetailApiView.as_view(), name='user-post-detail'),
+    path('create/', UserPostCreateApiView.as_view(), name='user-post-create'),
+    path('<str:id>/update/', UserPostUpdateApiView.as_view(), name='user-post-update'),
+    path('<str:id>/delete/', UserPostDeleteApiView.as_view(), name='user-post-delete'),
+    path('all/', get_posts, name='all-posts'),
+    path('<str:slug>/comment/', get_comments, name='post-comments'),
     path('comment/', add_comment, name='add-comment'),
-    path('<str:slug>/', get_article, name='article-detail'),
+    path('<str:slug>/', get_post, name='post-detail'),
 ]
